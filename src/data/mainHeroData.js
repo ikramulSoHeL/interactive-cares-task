e4e558d202986980347b9283b42aa15d7d7df229
ikramul <ikramulsohel101@gmail.com>
2023-11-10 21:31:24 +0600

    updated

--- src/data/mainHeroData.js
+++ src/data/mainHeroData.js
@@ -1,31 +1,34 @@
 // import image1 from "../assets/heroImages/slide1.jpg";
-import image2 from "../assets/heroImages/slider2.jpg";
+// import image2 from "../assets/heroImages/slider2.jpg";
 // import image3 from "../assets/heroImages/slider3.jpg";
 // import image4 from "../assets/heroImages/slide4.jpg";
 // import image5 from "../assets/heroImages/slide-5.jpg";
 // import image6 from "../assets/heroImages/slide-6.jpg";
-import image7 from "../assets/heroImages/skide-7.jpg";
-import image8 from "../assets/heroImages/slide-8.jpg";
+// import image7 from "../assets/heroImages/skide-7.jpg";
+// import image8 from "../assets/heroImages/slide-8.jpg";
 
 const mainHeroData = [
   {
     id: 1,
     title: "Welcome to Trisgo",
     subTitle: "We are the best in business",
-    image: image7,
+    image:
+      "https://images.pexels.com/photos/3848886/pexels-photo-3848886.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
   },
   {
     id: 2,
     title: "We provide Best Services",
     subTitle: "Safty and Security is our first priority. ",
-    image: image2,
+    image:
+      "https://images.pexels.com/photos/11769785/pexels-photo-11769785.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
   },
   {
     id: 3,
     title: "Travel Anywhere",
     subTitle:
       "Well trained and experienced support team to provide you best services.",
-    image: image8,
+    image:
+      "https://images.pexels.com/photos/6768830/pexels-photo-6768830.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
   },
 ];
 
